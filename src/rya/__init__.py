bd19eb1bda029c302b8b474de8223b86310b1b08
Mahadi Xion <mhxions@gmail.com>
2025-10-30 03:29:12 +0100

    Avoid importing ._core_init in __int__

--- src/rya/__init__.py
+++ src/rya/__init__.py
@@ -1,10 +1,5 @@
-from properpath import ProperPath
-
-from ._core_init import get_logger
 from ._core_utils import LayerLoader
-from .names import AppIdentity
 
-LayerLoader.logger = ProperPath.default_err_logger = get_logger()
-LayerLoader._self_app_name = LayerLoader._app_name = AppIdentity.app_name
+LayerLoader._self_app_name = LayerLoader._app_name = "rya"
 
-__all__ = ["AppIdentity"]
+__all__ = []
